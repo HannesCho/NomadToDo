bf6474aa6402b7951906be8ff9a6f490a1891592
Hannes Cho <hoonsungcho11@gmail.com>
2021-12-20 20:34:55 +0100

    add and save todos

--- App.js
+++ App.js
@@ -1,12 +1,68 @@
 import { StatusBar } from "expo-status-bar";
-import { useState } from "react";
-import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import { useEffect, useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  TextInput,
+  ScrollView,
+  Alert,
+} from "react-native";
 import { theme } from "./colors";
+import { Fontisto } from "@expo/vector-icons";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
+const STORAGE_KEY = "@toDos";
 export default function App() {
   const [select, setSelect] = useState(true);
+  const [text, setText] = useState("");
+  const [toDos, setToDos] = useState({});
+  useEffect(() => {
+    loadToDos();
+  }, []);
   const travel = () => setSelect(false);
   const work = () => setSelect(true);
+  const onChangeText = (payload) => setText(payload);
+  const saveToDos = async (toSave) => {
+    await AsyncStorage.setItem(STORAGE_KEY, JSON.stringify(toSave));
+  };
+  const loadToDos = async () => {
+    try {
+      const jsonPayload = await AsyncStorage.getItem(STORAGE_KEY);
+      return jsonPayload != null ? setToDos(JSON.parse(jsonPayload)) : null;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const addToDo = () => {
+    if (text === "") {
+      return;
+    }
+    // save to do
+    // const newToDos = Object.assign({}, toDos, {
+    //   [Date.now()]: { text, work: select },
+    // });
+    const newToDos = { ...toDos, [Date.now()]: { text, select } };
+    setToDos(newToDos);
+    saveToDos(newToDos);
+    setText("");
+  };
+  const deleteToDo = (key) => {
+    Alert.alert("Delete To Do", "Are you sure?", [
+      { text: "Cancel" },
+      {
+        text: "I'm Sure",
+        style: "destructive",
+        onPress: () => {
+          const newToDos = { ...toDos };
+          delete newToDos[key];
+          setToDos(newToDos);
+          saveToDos(newToDos);
+        },
+      },
+    ]);
+  };
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
@@ -26,6 +82,29 @@ export default function App() {
           </Text>
         </TouchableOpacity>
       </View>
+      <View>
+        <TextInput
+          onSubmitEditing={addToDo}
+          onChangeText={onChangeText}
+          placeholder={select ? "Add To Do!" : "Where do you want to go?"}
+          value={text}
+          style={styles.input}
+        />
+        <ScrollView>
+          {toDos
+            ? Object.keys(toDos).map((key) =>
+                toDos[key].select === select ? (
+                  <View key={key} style={styles.toDo}>
+                    <Text style={styles.toDoText}>{toDos[key].text}</Text>
+                    <TouchableOpacity onPress={() => deleteToDo(key)}>
+                      <Fontisto name="trash" size={18} color="red" />
+                    </TouchableOpacity>
+                  </View>
+                ) : null
+              )
+            : null}
+        </ScrollView>
+      </View>
     </View>
   );
 }
@@ -43,6 +122,29 @@ const styles = StyleSheet.create({
   },
   btnText: {
     fontSize: 50,
+    fontWeight: "600",
+  },
+  input: {
+    backgroundColor: "white",
+    paddingVertical: 15,
+    paddingHorizontal: 20,
+    borderRadius: 30,
+    marginVertical: 20,
+    fontSize: 18,
+  },
+  toDo: {
+    backgroundColor: theme.toDoBg,
+    marginBottom: 10,
+    paddingVertical: 20,
+    paddingHorizontal: 20,
+    borderRadius: 15,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  toDoText: {
+    color: "white",
+    fontSize: 16,
     fontWeight: "600",
   },
 });
